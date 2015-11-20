22b6e5b9c54cd13f56276fbb58ee6576e542181a
James Zarakas <james.zarakas@capitalone.com>
2015-11-20 18:45:38 -0500

    initial commit

---
+++ BigKeyboard/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  BigKeyboard
+//
+//  Created by Zarakas, James on 11/20/15.
+//  Copyright © 2015 Zarakas, James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var Buffer: UILabel!
+    
+    @IBOutlet weak var ZButton: UIButton!
+    @IBOutlet var BUTTONS: Array<UIButton>?
+    
+    @IBOutlet weak var BackButton: UIButton!
+    let ALPHABET :NSString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupButtons()
+    }
+    
+    func setupButtons() {
+        var i = 0
+        for b in BUTTONS! {
+            b.setTitle(ALPHABET.substringWithRange(NSRange(location: i, length: 5)), forState: .Normal)
+            i = i + 5
+        }
+        //when button 4 is inside the loop with a hard coded fix to add z, it kept adding z every time back was pressed.
+        BUTTONS![4].setTitle(ALPHABET.substringWithRange(NSRange(location: 20, length: 6)), forState: .Normal)
+        ZButton.hidden = true
+        BackButton.hidden = true
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onClear(sender: AnyObject) {
+        Buffer.text = ""
+    }
+    
+    @IBAction func onBackspace(sender: AnyObject) {
+        let words :NSString = Buffer.text!
+        if words.length == 0 {
+            return
+        }
+        Buffer.text = words.substringToIndex(words.length-1)
+    }
+    
+    @IBAction func onBack(sender: AnyObject) {
+        setupButtons()
+    }
+    
+    
+    @IBAction func onTap(sender: AnyObject) {
+        
+        let x: UIButton = sender as! UIButton
+        
+        if (x.titleLabel!.text!.characters.count == 1) {
+            Buffer.text = Buffer.text! + x.titleLabel!.text!
+            return
+        }
+        
+        let letters = x.titleLabel?.text
+        var i = 0
+        for c in (letters?.characters)! {
+            BUTTONS![i].setTitle( "\(c)", forState: .Normal)
+            i = i + 1
+            if i == BUTTONS?.count {
+                break
+            }
+        }
+        ZButton.hidden = false
+        BackButton.hidden = false
+        
+        
+    }
+    
+    
+    
+}
+
